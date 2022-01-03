6337884754198dd35380c0694389fa62ec304714
universitywits <84776816+universitywits@users.noreply.github.com>
2022-01-04 02:04:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID")
+    API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
